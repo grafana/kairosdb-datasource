a69a00cc372f94c4cd1c9df41374b392dd121624
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-07 10:39:29 +0200

    dropdown for tags

--- query_ctrl.js
+++ query_ctrl.js
@@ -13,12 +13,17 @@ function (angular, _, sdk) {
     function KairosDBQueryCtrl($scope, $injector, $timeout) {
       _super.call(this, $scope, $injector);
 
+      var that = this;
 
       this.tags = {
         tag1: ["val1","val2","val3","val4"],
         tag2: ["val5","val10","val11","val16"],
         tag3: ["val6","val9","val12","val15"],
         tag4: ["val7","val8","val13","val14"]
+      };
+
+      this.getNewTags = function(value) {
+        return that.tags.tag1;
       };
 
 
