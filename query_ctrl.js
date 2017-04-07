3cc4096e4f546e294c95abef5313e5a02f90dabc
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-07 10:21:31 +0200

    ng-repeat for tags

--- query_ctrl.js
+++ query_ctrl.js
@@ -13,6 +13,15 @@ function (angular, _, sdk) {
     function KairosDBQueryCtrl($scope, $injector, $timeout) {
       _super.call(this, $scope, $injector);
 
+
+      this.tags = {
+        tag1: ["val1","val2","val3","val4"],
+        tag2: ["val5","val10","val11","val16"],
+        tag3: ["val6","val9","val12","val15"],
+        tag4: ["val7","val8","val13","val14"]
+      };
+
+
       this.$timeout = $timeout;
       if (!this.target.downsampling) {
         this.target.downsampling = 'avg';
