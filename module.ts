be28a32fcb4420aced40219ff635bf50e35774ea
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-08 10:11:52 +0200

    loading plugin css

--- module.ts
+++ module.ts
@@ -1,5 +1,14 @@
 import {KairosDBDatasource} from './datasource';
 import {KairosDBQueryCtrl} from './query_ctrl';
+import {loadPluginCss} from 'app/plugins/sdk';
+
+loadPluginCss({
+    dark: 'app/plugins/datasource/grafana-kairosdb-datasource/css/plugin.css',
+    light: 'app/plugins/datasource/grafana-kairosdb-datasource/css/plugin.css'
+});
+
+//todo: only plugins
+//todo: import from /var/grafana/plugins/ dir
 
 class KairosDBConfigCtrl {
   static templateUrl = "partials/config.html";
