d42cb4d2e57c8eeeeea6ce6fd35ce91a42ef07e1
jsabin <jsabin@proofpoint.com>
2018-07-30 14:38:49 -0600

    Checked in changes to dist directory.

--- dist/utils/time_unit_utils.js
+++ dist/utils/time_unit_utils.js
@@ -29,6 +29,23 @@ System.register(["lodash", "../beans/aggregators/utils"], function(exports_1) {
                 TimeUnitUtils.getString = function (unit) {
                     return utils_1.TimeUnit[unit];
                 };
+                TimeUnitUtils.convertFromInterval = function (intervalString) {
+                    var interval_regex = /(\d+(?:\.\d+)?)([Mwdhmsy])/;
+                    var interval_regex_ms = /(\d+(?:\.\d+)?)(ms)/;
+                    var matches = intervalString.match(interval_regex_ms);
+                    if (!matches) {
+                        matches = intervalString.match(interval_regex);
+                    }
+                    if (!matches) {
+                        throw new Error('Expecting a number followed by one of "y M w d h m s ms"');
+                    }
+                    var value = matches[1];
+                    var unit = TimeUnitUtils.convertTimeUnit(matches[2]);
+                    return {
+                        value: value,
+                        unit: unit
+                    };
+                };
                 TimeUnitUtils.TIME_UNIT_STRINGS = lodash_1.default.values(utils_1.EnumValues(utils_1.TimeUnit));
                 TimeUnitUtils.SHORT_UNITS = lodash_1.default.zipObject(["ms", "s", "m", "h", "d", "w", "M", "y"], TimeUnitUtils.TIME_UNIT_STRINGS);
                 TimeUnitUtils.LONG_UNITS = lodash_1.default.zipObject(["millisecond", "second", "minute", "hour", "day", "week", "month", "year"], TimeUnitUtils.TIME_UNIT_STRINGS);
