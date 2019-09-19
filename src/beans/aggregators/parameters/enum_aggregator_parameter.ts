d9b6ddcd3975a78bfa7824c19f50d0c06b20ed52
Christian Briones <cwbriones@gmail.com>
2019-09-19 12:06:57 -0700

    tslint

--- src/beans/aggregators/parameters/enum_aggregator_parameter.ts
+++ src/beans/aggregators/parameters/enum_aggregator_parameter.ts
@@ -2,11 +2,11 @@ import {EnumValues} from "../utils";
 import {LimitedAggregatorParameter} from "./limited_aggregator_parameter";
 
 export class EnumAggregatorParameter extends LimitedAggregatorParameter {
-    private readonly enumType: any;
-
     public static fromObject(object: any): EnumAggregatorParameter {
         return new EnumAggregatorParameter(object.name, object.enumType, object.text, object.value);
     }
+
+    private readonly enumType: any;
 
     constructor(name: string, enumType: any, text: string = name, value: any = null) {
         super(name, EnumValues(enumType), text, value);
