0908c28b5666f3cec2440dbf1913dae86422257a
Christian Briones <cwbriones@gmail.com>
2019-09-19 12:21:53 -0700

    pass allowedValues instead of storing the enum type

--- src/beans/aggregators/parameters/enum_aggregator_parameter.ts
+++ src/beans/aggregators/parameters/enum_aggregator_parameter.ts
@@ -3,14 +3,11 @@ import {LimitedAggregatorParameter} from "./limited_aggregator_parameter";
 
 export class EnumAggregatorParameter extends LimitedAggregatorParameter {
     public static fromObject(object: any): EnumAggregatorParameter {
-        return new EnumAggregatorParameter(object.name, object.enumType, object.text, object.value);
+        return new EnumAggregatorParameter(object.name, object.allowedValues, object.text, object.value);
     }
-
-    private readonly enumType: any;
 
     constructor(name: string, enumType: any, text: string = name, value: any = null) {
         super(name, EnumValues(enumType), text, value);
-        this.enumType = enumType;
         this.type = "enum";
     }
 }
