dd3bce5d719a08025cfd0bf357fb4fb59da96e3d
Christian Briones <cwbriones@gmail.com>
2019-09-19 11:51:55 -0700

    camelCase instead of snake_case

--- src/beans/aggregators/parameters/enum_aggregator_parameter.ts
+++ src/beans/aggregators/parameters/enum_aggregator_parameter.ts
@@ -2,15 +2,15 @@ import {EnumValues} from "../utils";
 import {LimitedAggregatorParameter} from "./limited_aggregator_parameter";
 
 export class EnumAggregatorParameter extends LimitedAggregatorParameter {
-    private readonly enum_type: any;
+    private readonly enumType: any;
 
     public static fromObject(object: any): EnumAggregatorParameter {
-        return new EnumAggregatorParameter(object.name, object.enum_type, object.text, object.value);
+        return new EnumAggregatorParameter(object.name, object.enumType, object.text, object.value);
     }
 
-    constructor(name: string, enum_type: any, text: string = name, value: any = null) {
-        super(name, EnumValues(enum_type), text, value);
-        this.enum_type = enum_type;
+    constructor(name: string, enumType: any, text: string = name, value: any = null) {
+        super(name, EnumValues(enumType), text, value);
+        this.enumType = enumType;
         this.type = "enum";
     }
 }
